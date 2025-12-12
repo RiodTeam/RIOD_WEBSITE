a50efa3579c53c57a23bff1f53d8f029a7f4d63a
Fathima-Nahla <fathima.048@woxro.com>
2025-12-12 15:21:21 +0530

    individual blog page completed

---
+++ app/(route)/individual-insight/page.tsx
@@ -0,0 +1,16 @@
+
+
+import BlogDetailSection from '@/app/components/individual-insight/BlogDetailSection'
+import NextArticlesSection from '@/app/components/individual-insight/NextArticlesSection'
+
+
+const page = () => {
+  return (
+    <div>
+       <BlogDetailSection/>
+       <NextArticlesSection/>
+    </div>
+  )
+}
+
+export default page
